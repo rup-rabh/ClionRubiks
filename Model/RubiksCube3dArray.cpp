6969090fb9fcbb05e95bd51928ad0487e722b175
ruprabh <ruprabh@gmail.com>
2024-06-29 02:58:28 +0530

    fixedModulusIssueWow

--- Model/RubiksCube3dArray.cpp
+++ Model/RubiksCube3dArray.cpp
@@ -237,4 +237,40 @@ public:
         return *this;
     }
 
+	bool operator==(const RubiksCube3dArray &r1) const { //equality operator
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 3; k++) {
+					if (r1.cube[i][j][k] != cube[i][j][k]) return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	RubiksCube3dArray &operator=(const RubiksCube3dArray &r1) { //assignmment operator
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 3; k++) {
+					cube[i][j][k] = r1.cube[i][j][k];
+				}
+			}
+		}
+		return *this;
+	}
+};
+
+	struct Hash3d {
+		size_t operator()(const RubiksCube3dArray &r1) const { //hash Rule for cube
+			string str = "";
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < 3; j++) {
+					for (int k = 0; k < 3; k++) {
+						str += r1.cube[i][j][k];
+					}
+				}
+			}
+			return hash<string>()(str);
+	}
+
 };
--- main.cpp
+++ main.cpp
@@ -1,16 +1,60 @@
 #include <iostream>
-#include "Model/RubiksCube3dArray.cpp"
-
+#include "Model/RubiksCubeBitboard.cpp"
+// #include "Solver/BFSSolver.h"
+// #include "Solver/DFSSolver.h"
+#include "Solver/IDDFSSolver.h"
 int main()
 {
-    RubiksCube3dArray r1;
-    r1.print();
-    auto moves = r1.randomShuffleCube(1);
-    for (auto move: moves)
-    {
-        cout << r1.getMove(move)<< " ";
-    }
-    r1.print();
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	//////////////////////////////////////////////
+    // test 1.3
+     // RubiksCubeBitboard r1,r2;
+     // r1.print();
+     // auto moves = r1.randomShuffleCube(3);
+     // for (auto move: moves)
+     // {
+     //     cout << r1.getMove(move)<< " ";
+     // }
+	// cout<<(r1==r2)<<"\n";
+     // r1.print();
+	// unordered_map<RubiksCubeBitboard,bool,HashBitboard>mp1;
+	// mp1[r1] = true;
+	// cout<<mp1[r2]<<"\n";
+
+
+
+
+
+
+
+
+		///////////////----------SOLVERS TESTING--------//////////////////////////////
+
+	RubiksCubeBitboard r1;
+	r1.print();
+	auto movesToShuffle = r1.randomShuffleCube(3);
+	r1.print();
+	for(auto& move: movesToShuffle)cout << r1.getMove(move)<< " ";
+	cout<<"\n";
+
+	/////////////////////////////////DFS Works Cool
+	// DFSSolver<RubiksCubeBitboard,HashBitboard>dfs_solver(r1,6);
+	// auto movesToSovle = dfs_solver.solve();
+	// dfs_solver.rubiksCube.print();
+	// for(auto& move: movesToSovle)cout << r1.getMove(move)<< " ";
+	// cout<<"\n";
+
+
+	///////////////////////BFS Works Cool too
+	// BFSSolver<RubiksCubeBitboard,HashBitboard> bfs_solver(r1);
+	// auto movesToSovle = bfs_solver.solve();
+	// for(auto& move: movesToSovle)cout << r1.getMove(move)<< " ";
+	// cout<<"\n";
+	//////////////////IDDFS works cool
+	IDDFSSolver<RubiksCubeBitboard,HashBitboard> iddfs_solver(r1);
+	auto movesToSovle  = iddfs_solver.solve();
+	for(auto& move: movesToSovle)cout << r1.getMove(move)<< " ";
+	cout<<"\n";
+
+
+	return 0;
 }
